79b07ab78503e1654df46090ac14abf148cd2b80
duncan@lyrebirdhealth.com <duncanlyrebirdhealth.com@duncanlyrebirdhealthcoms-MacBook-Pro.local>
2025-09-04 18:57:24 +1000

    Initial commit of MLLP HL7 integration project

--- sendHL7.js
+++ sendHL7.js
@@ -10,7 +10,12 @@ const CR = String.fromCharCode(0x0d); // <CR>
 
 // Sample HL7 message (replace with your own if needed)
 const hl7Message = [
-  'MSH|^~\&|HIS|RLC|||20250902||ADT^A05|16|P|2.4|||AL|NE|||||| EVN|A05||||||| PID|1||AH1234567^^^^MR^RLC~123 456 7891^^^NHS^NH^RLC~^^^^PI^RLC~||Surname^First Name^^^^^L||20250101|F|||TigersHospital^^Auchtenlectalbert^^5AB 2JF^^P||07777777777~|||||V00007605635|123 456 7891|||||||||||||||||||| PD1|||^^N82053^AINTREE PARK GROUP PRACTICE|G8503501^HUBBERT CM^^| NK1|1|Test^Mum|M^|ALDER HEY^^LIVERPOOL^MERSEYSIDE^L14 5AB|07777777777||| PV1|1|P|1.2 ENT||F||GKHONG^Khong^^Grace||ENTFELLOW3|ENT||||||||CLI||||ENTFELLOW3|||||||||||||||||RLC||PRE||||||||||||',
+  'MSH|^~\\&|HIS|RLC|||20250902||ADT^A05|16|P|2.4|||AL|NE||||||',
+  'EVN|A05|||||||',
+  'PID|1||AH1234567^^^^MR^RLC~123 456 7891^^^NHS^NH^RLC~^^^^PI^RLC~||Surname^First Name^^^^^L||20250101|F|||TigersHospital^^Auchtenlectalbert^^5AB 2JF^^P||07777777777~|||||V00007605635|123 456 7891||||||||||||||||||||',
+  'PD1|||^^N82053^AINTREE PARK GROUPs PRACTICE|G8503501^HUBBERT CM^^|',
+  'NK1|1|Test^Mum|M^|ALDER HEY^^LIVERPOOL^MERSEYSIDE^L14 5AB|07777777777|||',
+  'PV1|1|P|1.2 ENT||F||GKHONG^Khong^^Grace||ENTFELLOW3|ENT||||||||CLI||||ENTFELLOW3|||||||||||||||||RLC||PRE||||||||||||',
 ].join('\r');
 
 // MLLP server config
